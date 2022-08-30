78b705b66c869ebd3d86f8886ab136d04c0ec9c4
roalejandro122 <38393490+roalejandro122@users.noreply.github.com>
2022-08-30 15:51:59 -0400

    primera clase
    asd

---
+++ playground.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+var n1 = 1
+var n2 = 2
+var n3 = 3
+var n4 = 4
+var n5 = 5
+var n6 = 6
+var n7 = 7
+var n8 = 8
+var n9 = 9
+var n10 = 10
+
+print("sumar dos numeros = ",n1+n2)
+print("restar dos numeros = ",n3+n4)
+print("multiplicar dos numeros = ",n5*n6)
+print("dividir dos numeros = ",n7/n8," y el resto de la divicion es: ",n7%n8)
+let str = readLine()
+print(str)
+
